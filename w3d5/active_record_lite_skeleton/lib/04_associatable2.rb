981c244cb8cb1eb474f6941d31d1dfd7a3b65ac1
Michael <michael.budnick@outlook.com>
2015-10-17 15:43:52 -0400

    Has many through w3d5

--- w3d5/active_record_lite_skeleton/lib/04_associatable2.rb
+++ w3d5/active_record_lite_skeleton/lib/04_associatable2.rb
@@ -5,6 +5,9 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through = self.send(through_name)
+      source = through.send(source_name)
+    end
   end
 end
